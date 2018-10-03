277b53a4fdb8ef5224b2bcc5d79122d4f772be0e
Mladen Zeljic <mladen_zeljic@hotmail.com>
2018-10-03 12:43:19 +0200

    Several additions, changes and improvements pt-5

--- index.php
+++ index.php
@@ -188,6 +188,6 @@
 	</div>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-	<script src="interface/scripts/index.js"></script>
+	<script src="interface/scripts/page.js"></script>
 </body>
 </html>
